76a70567ff5f4e61805945d4a82621ac3ea589ef
iskrzycki <iskrzycki@gmail.com>
2015-11-01 01:20:45 +0100

    Getting neighbours of provided cell
    function to get neighbours for selected cell. Also small changes in css

--- js/game.js
+++ js/game.js
@@ -2,13 +2,14 @@ function Cell(row, col) {
     this.row = row;
     this.col = col;
     this.isAlive = false;
+    this.nextIsAlive = false;
 }
 
 var CONFIG = {
     aliveCellColor: "yellow",
     deadCellColor: "black",
-    rows: 20,
-    cols: 40
+    rows: 40,
+    cols: 60
 };
 
 var GameOfLife = {
@@ -35,6 +36,7 @@ var GameOfLife = {
         var col = clickedCell.parent().children().index(clickedCell);
         var row = clickedCell.parent().parent().children().index(clickedCell.parent());
         this.updateCell(this.getCell(row, col));
+        this.getNeighbours(this.getCell(row, col));
     },
     updateCell: function (cell) {
         var table = document.getElementById("board");
@@ -49,6 +51,30 @@ var GameOfLife = {
         }
     },
     getCell: function (row, col) {
-        return this.cells[CONFIG.cols * row + (col % CONFIG.cols)];
+        if (row >= 0 && row < CONFIG.rows && col >= 0 && col < CONFIG.cols) {
+            return this.cells[CONFIG.cols * row + (col % CONFIG.cols)];
+        } else {
+            return null;
+        }
+    },
+    getNeighbours: function (cell) {
+        var cellArray = [];
+
+        cellArray.push(this.getCell(cell.row - 1, cell.col));
+        cellArray.push(this.getCell(cell.row, cell.col - 1));
+        cellArray.push(this.getCell(cell.row - 1, cell.col - 1));
+        cellArray.push(this.getCell(cell.row + 1, cell.col));
+        cellArray.push(this.getCell(cell.row, cell.col + 1));
+        cellArray.push(this.getCell(cell.row + 1, cell.col + 1));
+        cellArray.push(this.getCell(cell.row + 1, cell.col - 1));
+        cellArray.push(this.getCell(cell.row - 1, cell.col + 1));
+
+        for (var i = cellArray.length; i--;) {
+            if (cellArray[i] === null) {
+                cellArray.splice(i, 1);
+            }
+        }
+
+        return cellArray;
     }
 };
